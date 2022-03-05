5624d0fb8d9fb628d6af6a28a824a25ef58ea630
Steven Yang <yang558@wisc.edu>
2022-03-04 23:39:36 -0600

    Finished five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+mkdir five
+
+for i in $(seq 1 5); do
+    mkdir "five/dir${i}"
+    for j in $(seq 1 4); do
+	touch "five/dir${i}/file${j}"
+	for k in $(seq $j); do
+	    echo $j >> "five/dir${i}/file${j}"
+	done
+    done
+done
