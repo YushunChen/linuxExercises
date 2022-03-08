70f0b07d65646a4714ad25eeaa35e3d40908c2c2
Sirui Chu <schu46@wisc.edu>
2022-03-07 23:19:12 -0600

    Finished mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,39 @@
 #!/bin/bash
+
+function mean {
+	if [[ $# -lt 1 ]];
+	then 
+		echo "usage: $0 <column> [file.csv]" 1>&2;
+		exit 0;
+	fi;
+
+	if [[ $# -eq 1 ]];
+	then
+		dfile=/dev/stdin;
+	else
+		dfile=$2;
+	fi;
+
+	local col=$1;
+	local file=$dfile;
+
+
+	touch new_file.csv;
+	file_1="new_file.csv";
+	
+	
+	tail -n +2 "$file" | cut -d , -f $col > $file_1;
+
+	count=0;
+	sum=0;
+		
+	while read -r line;
+	do 
+		sum=$(($sum+$line));
+		count=$(($count+1));
+	done < $file_1;
+	mean=$(echo "$sum / $count" | bc -l);
+	echo $mean;
+	rm new_file.csv;
+}
+mean $1 $2
